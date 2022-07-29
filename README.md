54aea7e22f24687b1a954b253ea4c1feceb6b3f7
Milton <miltoncandelero@gmail.com>
2022-07-29 14:33:41 -0300

    initial version

---
+++ README.md
@@ -0,0 +1,20 @@
+# Killabunnies Code Style
+
+Killabunnies has a unique taste of coding style, which inspired me to create an ESLint config
+that can be installed for any project. This is created for any Typescript project and conventions should be injected afterwards.
+
+## Install
+
+```
+npm install -D @killabunnies/eslint-config
+```
+
+## Use
+
+```js
+{
+    "extends": [
+        "@killabunnies/eslint-config"
+    ]
+}
+```
---
+++ index.js
@@ -0,0 +1,69 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: "es2020", // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    tsconfigRootDir: "./",
+    project: './tsconfig.json',
+
+  },
+  plugins: [
+    '@typescript-eslint',
+
+  ],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    "plugin:prettier/recommended",
+  ],
+
+  rules: {
+    "spaced-comment": ["warn", "always", { "block": { "balanced": true, "exceptions": ["/"] }, }],
+    "@typescript-eslint/no-unsafe-return": "off",
+
+
+    "@typescript-eslint/explicit-member-accessibility": ["error", { "overrides": { constructors: 'off' } }],
+    "@typescript-eslint/explicit-function-return-type": ["error", { allowExpressions: true }],
+
+    //This one was HARD to write. This should enforce pretty much all the naming conventions...
+    "@typescript-eslint/naming-convention": [
+      "error",
+      { selector: "memberLike", modifiers: ['static'], format: ["UPPER_CASE", "camelCase"], leadingUnderscore: "forbid", trailingUnderscore: "forbid" }, //static members can be UPPER_CASE
+      { selector: "memberLike", modifiers: ['private', 'readonly'], format: ["UPPER_CASE", "camelCase"], leadingUnderscore: "allow", trailingUnderscore: "forbid" }, //static members can be UPPER_CASE
+      { selector: "memberLike", modifiers: ['static', 'private'], format: ["UPPER_CASE", "camelCase"], leadingUnderscore: "allow", trailingUnderscore: "forbid" }, //need to duplicate the rule above
+      { selector: "memberLike", modifiers: ["private"], format: ["camelCase"], leadingUnderscore: "allow", trailingUnderscore: "forbid" }, //privates can start with underscore only if they shadow fields
+      { selector: "variable", modifiers: ['const'], format: ["UPPER_CASE", "camelCase"], leadingUnderscore: "forbid", trailingUnderscore: "forbid" }, //const global variables can be UPPER_CASE
+
+      { selector: "variableLike", format: ["camelCase"], leadingUnderscore: "forbid", trailingUnderscore: "forbid" }, //variables have to be camel
+      { selector: "memberLike", format: ["camelCase"], leadingUnderscore: "forbid", trailingUnderscore: "forbid" }, //members have to be camel
+
+
+      { selector: "parameter", format: ["camelCase"], leadingUnderscore: "allow", trailingUnderscore: "forbid" }, //privates can start with underscore only if they shadow fields
+      { selector: "enumMember", format: ["UPPER_CASE"], leadingUnderscore: "forbid", trailingUnderscore: "forbid" }, //Enums Memebers are UPPER_CASE
+      { selector: "typeLike", format: ["PascalCase"], leadingUnderscore: "forbid", trailingUnderscore: "forbid" }, // Types are PascalCase
+    ],
+
+    "no-implicit-coercion": "error",
+
+    "curly": "error",
+    "@typescript-eslint/no-floating-promises": "off",
+    "prefer-template": 1,
+    "template-curly-spacing": [1, "never"],
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/unbound-method": "off", //pixijs binds differently to .bind
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "prettier/prettier": "warn",
+
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        "checksVoidReturn": false
+      }
+    ]
+
+  },
+
+};
